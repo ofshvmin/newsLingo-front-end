fd96a3bd67741ec2e861e920dc2271a7448416e0
Eunice Yang <euniceyang21@gmail.com>
2023-05-13 15:02:17 -0500

    update landing page message

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,25 +5,21 @@ import { NavLink } from 'react-router-dom'
 import styles from './NavBar.module.css'
 
 const NavBar = ({ user, handleLogout }) => {
-  const publicLinks = (
-    <ul>
-      <li><NavLink to="/auth/login">Log In</NavLink></li>
-      <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-    </ul>
-  )
-
-  const protectedLinks = (
-    <ul>
-      <li>Welcome, {user.name}</li>
-      {/* <li><NavLink to="/profiles">Profiles</NavLink></li> */}
-      <li><NavLink to="" onClick={handleLogout}>Log Out</NavLink></li>
-      {/* <li><NavLink to="/auth/change-password">Change Password</NavLink></li> */}
-    </ul>
-  )
-
   return (
     <nav className={styles.container}>
-      {user ? protectedLinks : publicLinks}
+      {user ?
+        <ul>
+          <li>{user.name}</li>
+          {/* <li><NavLink to="/profiles">Profiles</NavLink></li> */}
+          <li><NavLink to="" onClick={handleLogout}>Log Out</NavLink></li>
+          {/* <li><NavLink to="/auth/change-password">Change Password</NavLink></li> */}
+        </ul>
+      :
+        <ul>
+          <li><NavLink to="/auth/login">Log In</NavLink></li>
+          <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
+        </ul>
+      }
     </nav>
   )
 }
