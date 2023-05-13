7c54e76a31e16d89d9a137c9aad9881446e153c4
Eunice Yang <euniceyang21@gmail.com>
2023-05-13 14:33:31 -0500

    add NavBar css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,28 @@
 // npm modules
 import { NavLink } from 'react-router-dom'
 
-const NavBar = ({ user, handleLogout }) => {
+// css
+import styles from './NavBar.module.css'
 
+const NavBar = ({ user, handleLogout }) => {
   const publicLinks = (
     <ul>
       <li><NavLink to="/auth/login">Log In</NavLink></li>
       <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-  </ul>
+    </ul>
   )
 
   const protectedLinks = (
     <ul>
       <li>Welcome, {user.name}</li>
       {/* <li><NavLink to="/profiles">Profiles</NavLink></li> */}
-      <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
+      <li><NavLink to="" onClick={handleLogout}>Log Out</NavLink></li>
       {/* <li><NavLink to="/auth/change-password">Change Password</NavLink></li> */}
-  </ul>
+    </ul>
   )
 
   return (
-    <nav>
+    <nav className={styles.container}>
       {user ? protectedLinks : publicLinks}
     </nav>
   )
