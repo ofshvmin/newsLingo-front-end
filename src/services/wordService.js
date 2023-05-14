aa8fde900883fd2f09a3a13ea2610fc0ef47513d
Chris MacNamara <cpmacnamara@gmail.com>
2023-05-14 17:45:37 -0400

    refactor translation call to request from backend

--- src/services/wordService.js
+++ src/services/wordService.js
@@ -1,15 +1,8 @@
-// services
-import * as tokenService from './tokenService'
-
-//const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/articles`
-
-const BASE_URL = `https://www.dictionaryapi.com/api/v3/references/spanish/json/`
-
-const API_KEY = `?key=d1324e2a-6635-4b3a-8ad3-ecb3042b3277`
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/words/wordLookup`
 
 async function getTranslationFromAPI(query) {
   try {
-    const res = await fetch(`${BASE_URL}/${query}${API_KEY}`)
+    const res = await fetch(`${BASE_URL}?query=${query}`)
     return res.json()
   } catch (err) {
     console.log(err);
