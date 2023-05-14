53ac253400741152ac014c2471d24e488c9783b2
Eunice Yang <euniceyang21@gmail.com>
2023-05-14 17:12:08 -0500

    Build EditComment component

---
+++ src/pages/EditComment/EditComment.jsx
@@ -0,0 +1,48 @@
+// npm modules
+import { useState } from "react"
+import { useLocation, useParams, useNavigate } from "react-router-dom"
+
+// services
+import * as articleService from '../../services/articleService'
+
+//css
+import styles from './EditComment.module.css'
+
+const EditComment = () => {
+  const {state} = useLocation()
+  const {articleId, commentId} = useParams()
+  const navigate = useNavigate()
+
+  const [formData, setFormData] = useState(state)
+
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault()
+    await articleService.updateComment(articleId, commentId, formData)
+    navigate(`/articles/${articleId}`)
+  }
+
+  return (
+    <main className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <h1>Edit Comment</h1>
+        <label htmlFor="text-input">Text</label>
+        <textarea
+          required
+          type="text"
+          name="text" 
+          id="text-input"
+          value={formData.text}
+          placeholder="Text"
+          onChange={handleChange}
+        />
+        <button type="submit">Save</button>
+      </form>
+    </main>
+  )
+}
+
+export default EditComment
