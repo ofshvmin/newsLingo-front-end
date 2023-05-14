6ec6aced44e5cc7c861a282d8d038fc4f823b06a
Eunice Yang <euniceyang21@gmail.com>
2023-05-14 14:55:20 -0500

    Build Comments component

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,10 +1,21 @@
+import CommentCard from "../CommentCard/CommentCard"
 
 // css
 import styles from './Comments.module.css'
 
-const Comments = () => {
-  return (
+const Comments = (props) => {
+  if (!props.comments) return <h4>No Comments</h4>
 
+  return (
+    <>
+      {props.comments.map(comment => (
+        <CommentCard
+          key={CommentCard._id}
+          comment={comment}
+          user={props.user}
+        />
+      ))}
+    </>
   )
 }
 
