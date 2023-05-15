ddc7f1ef08106b1b2abdb1efde44ddc6b40c4dbd
Daniel Mathews <daniel.g.mathews@gmail.com>
2023-05-15 17:22:35 -0400

    style personal dictinoary

--- src/components/WordCard/WordCard.jsx
+++ src/components/WordCard/WordCard.jsx
@@ -1,17 +1,29 @@
 // css
 import styles from './WordCard.module.css'
+import { useState } from 'react'
 
 
 const WordCard = ({word, handleDeleteWord}) => {
+  const [showDefinition, setShowDefinition] = useState(false)
+
+  const handleWordCardClick = () => {
+    setShowDefinition(!showDefinition)
+  }
   return (
-    <main className={styles.container}>
-      <h1>{word.word}</h1>
+    <div className={styles.container}>
+      {/* <h1>{word.word}</h1>
       {word.translation.map((translation,idx) => 
         <h3 key={idx}>{translation}</h3>
         )}
-      <h1>{word.partOfSpeech}</h1>
+      <h4>{word.partOfSpeech}</h4> */}
+    {showDefinition ? 
+    <p>yes  </p> : 
+    
+    
+    <h1>{word.word}</h1>}
+
       <button onClick={()=>handleDeleteWord(word._id)}>Delete</button>
-    </main>
+      </div>
   )
 }
 
