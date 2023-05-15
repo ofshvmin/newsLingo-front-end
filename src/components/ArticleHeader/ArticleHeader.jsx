64869a4edc8a62cf40e75e199575ec9c83b2e2c2
Eunice Yang <euniceyang21@gmail.com>
2023-05-15 15:26:02 -0500

    Style ArticleHeader

--- src/components/ArticleHeader/ArticleHeader.jsx
+++ src/components/ArticleHeader/ArticleHeader.jsx
@@ -2,12 +2,13 @@
 import styles from './ArticleHeader.module.css'
 
 const ArticleHeader = (props) => {
+
   return (
     <header className={styles.container}>
-      <h4>{props.category}</h4>
+      <h3>{props.category[0].toUpperCase()}</h3>
       <h1>{props.title}</h1>
-      <h5>{props.pubDate}</h5>
-      <h5>By {props.creator}</h5>
+      <h4>{props.pubDate}</h4>
+      <h4>{props.creator}</h4>
       <img src={props.image_url} alt={props.title} />
     </header>
   )
