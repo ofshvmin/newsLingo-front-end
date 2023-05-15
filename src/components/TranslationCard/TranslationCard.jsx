c38301b7cb6f1e5459de0e6d71e0d5cd56b60afb
Chris MacNamara <cpmacnamara@gmail.com>
2023-05-15 11:15:51 -0400

    add form to translation card

--- src/components/TranslationCard/TranslationCard.jsx
+++ src/components/TranslationCard/TranslationCard.jsx
@@ -6,12 +6,26 @@ const TranslationCard = (props) => {
   console.log('Translation props', props);
   if(!props.translation[0].translations) return <p>No translation</p>
 
+  const translations = props.translation[0].translations.join('\n')
+
   return (
     <div>
-      {props.translation[0]?.queryWord}
-      {props.translation[0].translations.map((translation, idx) => (
-        <p key={idx}>{translation}</p>
-      ))}
+      <form action="">
+        <input 
+          type="text" 
+          value={props.translation[0]?.queryWord}
+          disabled={true}
+        />
+        <textarea 
+          readOnly 
+          name="" 
+          id="" 
+          cols="30" 
+          rows="10" 
+          value={translations}>
+        </textarea>
+        <button type="submit">Favorite</button>
+      </form>
     </div>
   )
 }
