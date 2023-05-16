bfc9185c4cd3bf18d3240b480b2d6fc59e597496
Chris MacNamara <cpmacnamara@gmail.com>
2023-05-15 23:07:51 -0400

    add country to article card

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -6,8 +6,11 @@ import styles from './ArticleCard.module.css'
 import articleImgPlaceholder from '../../assets/icons/no-image-icon.png'
 
 const ArticleCard = ({article}) => {
+  console.log('Article', article);
   const creator = article.creator ? article.creator[0] : "NO CREATOR FOUND"
   const image_url = article.image_url ? article.image_url : articleImgPlaceholder
+
+  const country = article.country ? article.country[0] : ''
 
   return (  
     <Link to={`/articles/${article._id}`}>
@@ -15,6 +18,10 @@ const ArticleCard = ({article}) => {
         <img src={image_url} alt={article.title} />
         <header>
           <h4>⏰ {article.pubDate.slice(0,10)}</h4>
+          {country.length ? 
+          <h4>{country.toUpperCase()}</h4>
+          : ''
+          }
           <h4>{creator}</h4>
           <h1>{article.title}</h1>
           <p>{article.description}</p>
