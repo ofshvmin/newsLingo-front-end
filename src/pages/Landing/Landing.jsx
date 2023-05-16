c306fcb98ff7a59dd6a7cfbb2d11be44dfb87d47
Chris MacNamara <cpmacnamara@gmail.com>
2023-05-16 12:03:25 -0400

    add image to landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,25 +1,26 @@
 // css
 import styles from './Landing.module.css'
 import logo from '../../assets/icons/logoPlaceholder.png'
+import logo2 from '../../assets/icons/logo2.png'
 
 const Landing = () => {
   return (
     <>
-    <main className={styles.container}>
+    <main className={styles.landingContainer}>
       <section className={styles.splash}>
-        <img src={logo} alt="NewsLingo" />
+        <img src={logo2} alt="NewsLingo" />
         <h1> Breaking News, Breaking Barriers </h1>
       </section>
 
       <section className={styles.about}>
-        <header>
-          <h1>What does NewsLingo solve?</h1>
-        </header>
-        <article>
-          <p>
-            News articles are a great source for practicing a new language and learning about the cultures of those who speak it. For novice and intermediate-level language learners, news articles can be challenging to interpret.
-          </p>
-        </article>
+          <header>
+            <h1>What does NewsLingo solve?</h1>
+          </header>
+          <article>
+            <p>
+              News articles are a great source for practicing a new language and learning about the cultures of those who speak it. For novice and intermediate-level language learners, news articles can be challenging to interpret.
+            </p>
+          </article>
       </section>
 
       <section className={styles.summary}>
@@ -30,7 +31,7 @@ const Landing = () => {
           <ul>
             <li>NewsLingo scaffolds learners’ interpretive reading with convenient word-to-word translations within authentic, up-to-date news articles.</li>
             <li>It boosts learners’ language acquisition with  a personal dictionary to keep track of newly learned vocabulary. </li>
-            <li>It allows learners to interact with a community via a comment section on each article.</li>
+            <li>It allows learners to interact with a community of language learners via a comment section on each article.</li>
           </ul>
         </article>
       </section>
