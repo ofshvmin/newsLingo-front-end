1bea07cd6f636599b0e3a8e623ee51d504148fc2
Chris MacNamara <cpmacnamara@gmail.com>
2023-05-17 15:55:53 -0400

    add categorizedArticles object

--- src/pages/ArticleList/ArticleList.jsx
+++ src/pages/ArticleList/ArticleList.jsx
@@ -5,6 +5,18 @@ import ArticleCard from "../../components/ArticleCard/ArticleCard"
 import styles from './ArticleList.module.css'
 
 const ArticleList = (props) => {
+  //console.log("ARTICLE LIST", props.articles);
+
+  const categorizedArticles = {};
+  props.articles.forEach(article => {
+    if(categorizedArticles[article.category[0]]) 
+      categorizedArticles[article.category[0]].push(article)
+    else
+      categorizedArticles[article.category[0]] = [article]
+  })
+
+  console.log("CAT ARTICLES", categorizedArticles);
+
   return (
     <main className={styles.container}>
       {props.articles.map(article => (
