c2ee7440928db5ad667f4157e663a26abedb2093
Chris MacNamara <cpmacnamara@gmail.com>
2023-05-17 12:09:07 -0400

    fix hasNoRelatedWords and order of translation cards

--- src/pages/ArticleDetails/ArticleDetails.jsx
+++ src/pages/ArticleDetails/ArticleDetails.jsx
@@ -70,12 +70,12 @@ const ArticleDetails = (props) => {
       })
 
       console.log('Final translation parse: ', newWordTranslations);
-      setTranslations([newWordTranslations, ...translations])
+      setTranslations([...translations, newWordTranslations])
       console.log('Translations state', translations);
     } else {
       // merriam-webster returns alternative search suggestions when it can't find a given word
       console.log("Word not found. Alternate search suggestions:", data);
-      setTranslations([data, ...translations])
+      setTranslations([...translations, data])
 
     }
   }
