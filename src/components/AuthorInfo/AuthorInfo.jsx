0d813fbba5583607c333f01581a35f4af77ffcfb
Eunice Yang <euniceyang21@gmail.com>
2023-05-18 18:40:36 -0500

    Update profile icon to be consistent with other icons

--- src/components/AuthorInfo/AuthorInfo.jsx
+++ src/components/AuthorInfo/AuthorInfo.jsx
@@ -2,17 +2,15 @@
 import DateCard from '../DateCard/DateCard'
 
 // assets
-import profileImg from '../../assets/icons/profilePlaceholder.png'
+import profileIcon from '../../assets/icons/profile.svg'
 
 // css
 import styles from './AuthorInfo.module.css'
 
 const AuthorInfo = ({ content }) => {
-  const photo = content.author.photo ? content.author.photo : profileImg
-
   return (
     <div className={styles.authorInfo}>
-      <img src={photo} alt="The user's avatar" />
+      <img src={content.author.photo || profileIcon}/>
       <section>
         <h5>{content.author.name}</h5>
         <DateCard createdAt={content.createdAt} />
